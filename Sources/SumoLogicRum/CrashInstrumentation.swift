cda1991d932fc56da00e5a310d02cf3f8580f176
Krystian Kruk <kkruk@sumologic.com>
2022-04-13 13:33:46 +0200

    Send crash reports as traces

--- Sources/SumoLogicRum/CrashInstrumentation.swift
+++ Sources/SumoLogicRum/CrashInstrumentation.swift
@@ -1,10 +1,12 @@
 import Foundation
 import CrashReporter
+import OpenTelemetryApi
+import OpenTelemetrySdk
 
 class CrashInstrumentation {
     init(logsExporter: SumoLogicLogsExporter) {
         if getppid() != 1 {
-            // ppid=1 means that the debugger is running and we cannot initialize PLCrashReporter
+            print("SumoLogicRum crash instrumentation is disabled because running debugger was detected")
             return
         }
         
@@ -27,13 +29,33 @@ class CrashInstrumentation {
 
                 // Retrieving crash reporter data.
                 let report = try PLCrashReport(data: data)
-
-                // We could send the report from here, but we'll just print out some debugging info instead.
-                if let text = PLCrashReportTextFormatter.stringValue(for: report, with: PLCrashReportTextFormatiOS) {
-                    SumoLogicRum.recordError(message: text)
-                } else {
-                    print("CrashReporter: can't convert report to text")
+                let tracer = OpenTelemetrySDK.instance.tracerProvider.get(instrumentationName: "crash", instrumentationVersion: "0.0.1")
+                let span = tracer.spanBuilder(spanName: "crash").setNoParent().setStartTime(time: report.systemInfo.timestamp).startSpan()
+                var eventAttributes: [String: AttributeValue] = [
+                    "signalName": .string(report.signalInfo.name),
+                    "signalCode": .string(report.signalInfo.code),
+                ]
+                if report.hasProcessInfo {
+                    eventAttributes.merge([
+                        "processName": .string(report.processInfo.processName),
+                        "processID": .int(Int(report.processInfo.processID)),
+                        "processPath": .string(report.processInfo.processPath),
+                    ]) { (_, new) in new }
+                    if report.processInfo.processStartTime != nil {
+                        eventAttributes["processStartTime"] = .string(DateFormatter().string(from: report.processInfo.processStartTime))
+                    }
+                    if report.processInfo.parentProcessName != nil {
+                        eventAttributes["parentProcessName"] = .string(report.processInfo.parentProcessName)
+                    }
                 }
+                if report.hasExceptionInfo {
+                    eventAttributes.merge([
+                        "exceptionName": .string(report.exceptionInfo.exceptionName),
+                        "exceptionReason": .string(report.exceptionInfo.exceptionReason),
+                    ]) { (_, new) in new }
+                }
+                span.addEvent(name: "crash", attributes: eventAttributes, timestamp: report.systemInfo.timestamp)
+                span.end(time: report.systemInfo.timestamp.addingTimeInterval(1))
             } catch let error {
                 print("CrashReporter failed to load and parse with error: \(error)")
             }
